dde97712fe1408df9803f03632f56b25c85dfc85
Jordan Wallace <39134950+jordicious@users.noreply.github.com>
2018-05-11 08:23:51 -0400

    Quick UI and repo cleanup
    Switched app to use fullscreen window and cleaned up some old files in the repo folder

--- background.js
+++ background.js
@@ -1,11 +1,7 @@
 chrome.app.runtime.onLaunched.addListener(function() {
     chrome.app.window.create("index.html",
-        {  frame: "none",
-            id: "framelessWinID",
-            innerBounds: {
-                width: screen.availHeight,
-                height: screen.availHeight,
-               }
+        {
+            state: "fullscreen",
         }
     );
 });
