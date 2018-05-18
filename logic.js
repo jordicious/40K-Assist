2c458ac93157b0a5ebc1de50f1fed4ad22cf9b76
Jordan Wallace <39134950+jordicious@users.noreply.github.com>
2018-05-18 13:19:54 -0400

    Imroved Stability, JS upgrades, and JSON expansion
    Fixed a few nagging bugs
    Added subunit capability, similar to the way sub weapon selection works.
    Also added a ping for unit.models which sets the max on the modelsrange slider.  The slider is disabled if unit.models is not present.
    Added lots more T'au units.

--- logic.js
+++ logic.js
@@ -32,8 +32,13 @@ $(document).ready(function () {
     $('#unit').change(function () {
         var selUnit = $('#unit').val();
         var file = fileHead + 'roster/' + selUnit + '.json';
-        JSONRead(file, fillWargear)
+        JSONRead(file, pullsubunit);
         $("#unit option[value='none']").remove();   //removes the default -select- option
+    });
+
+    $("#subunit").change(function () {
+        var file = fileHead + '/roster/' + $("#subunit").val() + '.json';
+        JSONRead(file, fillWargear);
     });
 
     // monitors the wargear selector and calls the subtype checker
@@ -54,12 +59,10 @@ $(document).ready(function () {
     });
 
     $("#modelsrange").mousemove(function () {
-        console.log($('#modelsrange').val());
         $('#modelstext')[0].value = $('#modelsrange').val();
     });
 
     $("#modelsrange").change(function () {
-        console.log($('#modelsrange').val());
         $('#modelstext')[0].value = $('#modelsrange').val();
     });
 
@@ -67,8 +70,9 @@ $(document).ready(function () {
         $('#modelsrange')[0].value = $('#modelstext').val();
     });
 
-    // initial hide of the subweapon selector
+    // initial hide of the subselectors
     $('#subweaponcell').hide();
+    $('#subunitcell').hide();
 });
 
 function JSONRead(file, callback) {
@@ -94,16 +98,45 @@ function fillArmies(armies) {
 
 function populate(ros) {
     roster = ros.units;   //updates global list of available units
-    $('#unit')[0].innerHTML = '<option> -select- </option>';
+    $('#unit')[0].innerHTML = '<option value="none"> -select- </option>';
     for (var i = 0; i < roster.length; i++) {
         $('#unit')[0].innerHTML += '<option>' + roster[i] + '</option>';    //generates and appends option tags for each unit
     }
+    $('#subunitcell').hide();
+}
+
+function pullsubunit(unit) {   // checks if the selected weapon has overcharge or similar traits and applies the subselector
+    try {   //safe way of checking if the unit has the subs key in its JSON file
+        var subs = unit.subs;
+        $('#subunit')[0].innerHTML = ''; //clear the default option
+        for (var i = 0; i < subs.length; i++) {
+            var value = $('#unit').val();
+            if (subs[i] !== ''){
+                value += ' ' + subs[i];
+                var line = '<option value="' + value + '">' + subs[i] + '</option>';
+            } else {
+                var line = '<option value="' + value + '">none</option>';
+            }
+            $('#subunit')[0].innerHTML += line;
+        }
+        $('#subunitcell').show(); //unhides the td that contains the subselector
+
+    } catch (err) {
+        $('#subunitcell').hide(); //hides the td that contains the subselector
+        var value = unit.name;
+        var line = '<option value="' + value + '">' + value + '</option>';  //essentially all this bit does is push the value of the big selector to the little one
+                                                                            //this is so that we can use one function to get the name of the weapon we want to read from, and keep everything cleaner
+        $('#subunit')[0].innerHTML = line;
+    }
+    $('#subunit').change();    //listener trigger for the subselector
+    selUnit = $("#subunit").val();
+    $('#subweaponcell').hide();
 }
 
 function fillWargear(unit) {
     selUnit = unit; //updates the global selected unit object
     manifest = unit.weapons;    //updates the global list of available weapons
-    $('#weapon')[0].innerHTML = '<option> -select- </option>';
+    $('#weapon')[0].innerHTML = '<option value="none"> -select- </option>';
     for (var i = 0; i < manifest.length; i++) {
         $('#weapon')[0].innerHTML += '<option>' + manifest[i] + '</option>';    //generates and appends the names of all available weapons
     }
@@ -111,7 +144,7 @@ function fillWargear(unit) {
     var bs = unit.bs;   //this part of the function updates the values for the ballistic skill detector
     $('#slidebox')[0].innerHTML = ''; //clear the default option
     if (bs.length > 1) {     //simple check if ballistic value is an array
-        var line = '<td><input type="radio" name="bs" value=' + bs[i] + ' checked><label>' + bs[i] + '+</label></td>';  //generates the first cell for the table that holds the bs buttons - keep this out of the for loop to avoid issues
+        var line = '<td><input type="radio" name="bs" value=' + bs[0] + ' checked><label>' + bs[0] + '+</label></td>';  //generates the first cell for the table that holds the bs buttons - keep this out of the for loop to avoid issues
         $('#slidebox')[0].innerHTML += line;
         for (var i = 1; i < bs.length; i++) {
             var line = '<td><input type="radio" name="bs" value=' + bs[i] + '><label for="bs' + i + '">' + bs[i] + '+</label></td>';    //the rest of the ballistic skills radios
@@ -122,6 +155,17 @@ function fillWargear(unit) {
         $("#slidebox")[0].innerHTML = line;
     }
     bsTrack = true;
+
+    var models = selUnit.models;
+    if (models < 2 || ! models){
+        models = 1;
+        $("#modelsrange").addClass('grey');
+        $('#modelsrange')[0].max = models;
+    } else {
+        $("#modelsrange").removeClass('grey');
+        $('#modelsrange')[0].max = models;
+    }
+    $("#modelsrange").change();
 }
 
 function pullsubs(weap) {   // checks if the selected weapon has overcharge or similar traits and applies the subselector
@@ -167,8 +211,13 @@ $(document).ready(function () {
     $('#unit2').change(function () {
         var file = fileHead2 + 'roster/' + $('#unit2').val() + '.json';
         $("#unit2 option[value='none']").remove(); //removes the default -select- option
+        JSONRead(file, pullsubunit2);
+    });
+    $("#subunit2").change(function () {
+        var file = fileHead2 + 'roster/' + $('#subunit2').val() + '.json';
         JSONRead(file, readUnit2);
     });
+    $('#subunitcell2').hide();
 });
 
 function fillArmies2(armies) {
@@ -180,10 +229,38 @@ function fillArmies2(armies) {
 
 function populate2(ros) {
     roster2 = ros.units;
-    $('#unit2')[0].innerHTML = '<option> -select- </option>';
+    $('#unit2')[0].innerHTML = '<option value="none"> -select- </option>';
     for (var i = 0; i < roster2.length; i++) {
         $('#unit2')[0].innerHTML += '<option>' + roster2[i] + '</option>';
     }
+}
+
+function pullsubunit2(unit) {   // checks if the selected weapon has overcharge or similar traits and applies the subselector
+    try {   //safe way of checking if the unit has the subs key in its JSON file
+        var subs = unit.subs;
+        $('#subunit2')[0].innerHTML = ''; //clear the default option
+        for (var i = 0; i < subs.length; i++) {
+            var value = $('#unit2').val();
+            if (subs[i] !== ''){
+                value += ' ' + subs[i];
+                var line = '<option value="' + value + '">' + subs[i] + '</option>';
+            } else {
+                var line = '<option value="' + value + '">none</option>';
+            }
+            $('#subunit2')[0].innerHTML += line;
+        }
+        $('#subunitcell2').show(); //unhides the td that contains the subselector
+
+    } catch (err) {
+        $('#subunitcell2').hide(); //hides the td that contains the subselector
+        var value = unit.name;
+        var line = '<option value="' + value + '">' + value + '</option>';  //essentially all this bit does is push the value of the big selector to the little one
+                                                                            //this is so that we can use one function to get the name of the weapon we want to read from, and keep everything cleaner
+        $('#subunit2')[0].innerHTML = line;
+    }
+    $('#subunit2').change();    //listener trigger for the subselector
+    selUnit2 = $("#subunit2").val();
+    $('#subweaponcell2').hide();
 }
 
 function readUnit2(unit) {  //this function fills up saves for the active unit kind of like how the ballistic skill fills.  the only difference is that there can only be 1 or 2 saves, never 3.
@@ -196,7 +273,6 @@ function readUnit2(unit) {  //this function fills up saves for the active unit k
     $("#savebox")[0].innerHTML = line;
 
     if (sv.inv){
-        console.log("sv.inv is defined");
         var line = '<td><input type="radio" name="save" value=' + sv.inv + '><label>' + sv.inv + '++</label></td>';
         $('#savebox')[0].innerHTML += line;
         $('#type2')[0].innerText += " / " + sv.inv + "++"
